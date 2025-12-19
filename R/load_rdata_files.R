11ad6dbdf8a7c859db768a9ab3fa81303b513195
bgcasey <brendangerardcasey@gmail.com>
2025-12-19 13:45:47 -0700

    Update load_rdata_files documentation: correct @example to @examples and format usage

--- R/load_rdata_files.R
+++ R/load_rdata_files.R
@@ -8,12 +8,15 @@
 #' @return A list where each element is an object loaded from the
 #' `.RData` files.
 #'
-#' @example
+#' @examples
+#' \dontrun{
 #' # Example usage of the function
 #' data_dir <- "path/to/your/data/directory"
 #' all_data <- load_rdata_files(data_dir)
 #' print(names(all_data)) # Prints the names of loaded objects
+#' }
 #'
+#' @export
 load_rdata_files <- function(data_dir) {
     # Step 1: Get a list of all .RData files in the specified directory
     rdata_files <- list.files(
