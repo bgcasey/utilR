d441b122c88a9fa3faa16488c3cfe66f235b6321
bgcasey <brendangerardcasey@gmail.com>
2025-12-19 14:25:42 -0700

    Refactor add_alberta_flag function: update return type documentation and streamline code structure

--- R/add_alberta_flag.R
+++ R/add_alberta_flag.R
@@ -5,63 +5,58 @@
 #' @param dat Data frame with latitude and longitude columns
 #' @param lat_col Name of latitude column (default: "latitude")
 #' @param lon_col Name of longitude column (default: "longitude")
-#' @return Data frame with in_alberta column ("in" or "out")
+#' @return Data frame with in_alberta column (TRUE/FALSE logical)
 #' @import dplyr
 #' @import sf
 #' @import rnaturalearth
 #' @importFrom rlang .data
 #' @export
 add_alberta_flag <- function(
-    dat,
-    lat_col = "latitude",
-    lon_col = "longitude") {
-    # Load required packages
-    requireNamespace("sf")
-    requireNamespace("dplyr")
-    requireNamespace("rnaturalearth")
-    requireNamespace("rnaturalearthhires", quietly = TRUE)
+  dat,
+  lat_col = "latitude",
+  lon_col = "longitude") {
 
-    # Assign a temporary unique row identifier
-    dat$.row_id_tmp <- seq_len(nrow(dat))
+  # Assign a temporary unique row identifier
+  dat$.row_id_tmp <- seq_len(nrow(dat))
 
-    # Get Canada provinces polygons
-    can_prov <- rnaturalearth::ne_states(
-        country = "Canada",
-        returnclass = "sf"
+  # Get Canada provinces polygons
+  can_prov <- rnaturalearth::ne_states(
+    country = "Canada",
+    returnclass = "sf"
+  )
+
+  # Filter for Alberta
+  ab <- can_prov %>%
+    dplyr::filter(name_en == "Alberta")
+
+  # Convert input data to sf
+  dat_sf <- dat %>%
+    dplyr::filter(
+      !is.na(.data[[lat_col]]),
+      !is.na(.data[[lon_col]])
+    ) %>%
+    sf::st_as_sf(
+      coords = c(lon_col, lat_col),
+      crs = sf::st_crs(ab)
     )
 
-    # Filter for Alberta
-    ab <- can_prov %>%
-        dplyr::filter(name_en == "Alberta")
+  # Spatial join: flag if site is in Alberta
+  dat_sf$in_alberta <- sf::st_within(
+    dat_sf, ab,
+    sparse = FALSE
+  )[, 1]
 
-    # Convert input data to sf
-    dat_sf <- dat %>%
-        dplyr::filter(
-            !is.na(.data[[lat_col]]),
-            !is.na(.data[[lon_col]])
-        ) %>%
-        sf::st_as_sf(
-            coords = c(lon_col, lat_col),
-            crs = sf::st_crs(ab)
-        )
+  # Join back to original data using the unique row id
+  dat <- dat %>%
+    dplyr::left_join(
+      dat_sf %>%
+        dplyr::select(.row_id_tmp, in_alberta) %>%
+        sf::st_drop_geometry(),
+      by = ".row_id_tmp"
+    )
 
-    # Spatial join: flag if site is in Alberta
-    dat_sf$in_alberta <- sf::st_within(
-        dat_sf, ab,
-        sparse = FALSE
-    )[, 1]
+  # Remove the temporary identifier
+  dat$.row_id_tmp <- NULL
 
-    # Join back to original data using the unique row id
-    dat <- dat %>%
-        dplyr::left_join(
-            dat_sf %>%
-                dplyr::select(.row_id_tmp, in_alberta) %>%
-                sf::st_drop_geometry(),
-            by = ".row_id_tmp"
-        )
-
-    # Remove the temporary identifier
-    dat$.row_id_tmp <- NULL
-
-    return(dat)
+  return(dat)
 }
